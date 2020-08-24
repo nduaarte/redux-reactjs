a4f1200abee492a2860ed1b339f4a56ebfd9b74c
Nycollas <nycollas.duaarte@hotmail.com>
2020-08-24 18:35:30 -0300

    update files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import CourseList from './components/CourseList';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Provider store={store}>
+      <CourseList />
+    </Provider>
   );
 }
 
